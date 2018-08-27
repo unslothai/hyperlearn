9f5b234db4f08f592ff647d2108deb3859a9c0e2
Daniel Han-Chen <danielhanchen@gmail.com>
2018-08-28 02:44:17 +1000

    Add files via upload
    First Commit!

---
+++ base.py
@@ -0,0 +1,176 @@
+from torch import from_numpy as _toTensor, einsum as t_einsum, \
+                transpose as __transpose, Tensor as typeTensor, stack as __stack, \
+                float32, int32
+from functools import wraps    
+from numpy import finfo as np_finfo, einsum as np_einsum, log as np_log, array as np_array
+from numpy import float32 as np_float32, float64 as np_float64, int32 as np_int32, \
+                    int64 as np_int64, bool_ as np_bool, uint8 as np_uint8, ndarray
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
+from inspect import isclass as isClass
+
+use_numpy_einsum = True
+FloatInt = (float, int)
+ListTuple = (list, tuple)
+ArrayType = (np_array, ndarray)
+KeysValues = ( type({}.keys()), type({}.values()), dict )
+
+def isList(X):
+    return type(X) in ListTuple
+
+def isArray(X):
+    return type(X) in ArrayType
+
+def isIterable(X):
+    if isArray(X):
+        if len(X.shape) == 1: return True
+    return isList(X)
+
+def isDict(X):
+    return type(X) in KeysValues
+
+def array(X):
+    if isDict(X):
+        X = list(X)
+    return np_array(X)
+
+def toTensor(X):
+    if type(X) is typeTensor:
+        return X
+    if isDict(X):
+        X = list(X)
+    if isList(X):
+        X = np_array(X)
+    try:
+        if X.dtype == np_bool:
+            X = X.astype(np_uint8)
+        return _toTensor(X)
+    except:
+        return X
+
+def isTensor(X):
+    return type(X) is typeTensor
+
+
+def Tensor(*args):
+    out = []
+    for x in args:
+        if isTensor(x):
+            out.append(  x  )
+        else:
+            out.append(  toTensor(x)  )
+    return out
+
+
+def Numpy(*args):
+    out = []
+    if isList(args[0]):
+        args = args[0]
+
+    for x in args:
+        if isTensor(x):
+            out.append(  x.numpy()  )
+        else:
+            out.append(  x  )
+    return out
+
+
+def return_numpy(*args):
+    result = Numpy(*args)
+    if len(result) == 1:
+        return result[0]
+    else:
+        return tuple(result)
+
+
+def n2n(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if isClass(args[0]):
+            returned = f(args[0], *Tensor(*args[1:]), **kwargs)
+        else:
+            returned = f(*Tensor(*args), **kwargs)
+        
+        if not isList(returned):
+            returned = [returned]
+        
+        return return_numpy(*returned)
+    return wrapper
+
+
+def n2t(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if isClass(args[0]):
+            returned = f(args[0], *Tensor(*args[1:]), **kwargs)
+        else:
+            returned = f(*Tensor(*args), **kwargs)
+        return returned
+    return wrapper
+
+
+def einsum(notation, *args, tensor = False):
+    if use_numpy_einsum:
+        args = Numpy(*args)
+        out = np_einsum(notation, *args)
+    else:
+        args = Tensor(*args)
+        try:
+            out = t_einsum(notation, *args)
+        except:
+            out = t_einsum(notation, args)
+    if tensor:
+        return toTensor(out)
+    return out
+
+
+def T(X):
+    if not isTensor(X):
+        X = toTensor(X)
+    if len(X.shape) == 1:
+        return X.reshape(-1,1)
+    return __transpose(X, 0, 1)
+
+
+def ravel(y, X):
+    if isTensor(y):
+        return y.flatten().type( X.dtype )
+    else:
+        return toTensor(y.ravel().astype( X.dtype ))
+
+
+def row(y):
+    y = np_array(y).ravel()
+    try:
+        return toTensor(y).type( int32 )
+    except:
+        return y
+
+def row_np(y):
+    return np_array(y).ravel()
+
+
+def constant(X):
+    return X.item()
+
+
+def eps(X):
+    try:
+        return np_finfo(dtype(X)).eps
+    except:
+        return np_finfo(np_float64).eps
+    
+
+def dtype(tensor):
+    string = str(tensor.dtype)
+    if 'float32' in string: return np_float32
+    elif 'float64' in string: return np_float64
+    elif 'int32' in string: return np_int32
+    elif 'int64' in string: return np_int64
+    else: return np_float32
+    
+
+def stack(*args):
+    if isList(args[0]):
+        args = args[0]
+    toStack = Tensor(*args)
+    return __stack(toStack)
---
+++ linalg.py
@@ -0,0 +1,160 @@
+from .base import *
+from torch import svd as __svd, qr as __qr
+from scipy.linalg.lapack import clapack
+from scipy.stats import t as tdist
+
+__all__ = ['svd','_svd','pinv','_pinv','qr_solve','svd_solve',
+			'ridge_solve','qr_stats','svd_stats','ridge_stats']
+
+def t_svd(X, U = True):
+	if U:
+		U, S, V = __svd(X, some = True)
+		return U, S, T(V)
+	else:
+		__, S, V = __svd(X, some = True)
+		return S, T(V)
+svd = n2n(t_svd)
+_svd = n2t(t_svd)
+
+
+def t_pinv(X):
+    U, S, VT = _svd(X)
+    cond = S < eps(X)*constant(S[0])
+    _S = 1.0 / S
+    _S[cond] = 0.0
+    VT *= T(_S)
+    return T(VT).matmul(T(U))
+pinv = n2n(t_pinv)
+_pinv = n2t(t_pinv)
+
+
+def t_qr(X):
+    return __qr(X)
+qr = n2n(t_qr)
+_qr = n2t(t_qr)
+
+
+def qr_solve(X, y):
+    '''
+    theta =  R^-1 * QT * y
+    '''
+    Q, R = qr(X)
+    check = 0
+    if R.shape[0] == R.shape[1]:
+        _R, check = clapack.strtri(R)
+    if check > 0:
+        _R = _pinv(R)
+    Q, _R, R = Tensor(Q, _R, R)
+    
+    theta_hat = _R.matmul(   T(Q).matmul( ravel(y, Q) )   )
+    return theta_hat
+
+
+def svd_solve(X, y):
+    '''
+    theta =  V * S^-1 * UT * y
+    '''
+    U, S, VT = _svd(X)
+    cond = S < eps(X)*constant(S[0])
+    _S = 1.0 / S
+    _S[cond] = 0.0
+    VT *= T(_S)
+    
+    theta_hat = T(VT).matmul(  
+                            T(U).matmul(  ravel(y, U)  )
+                            )
+    return theta_hat
+
+
+def ridge_solve(X, y, alpha = 1):
+    '''
+                    S
+    theta =   V --------- UT y 
+                 S^2 + aI
+    '''
+    U, S, VT = _svd(X)
+    cond = S < eps(X)*constant(S[0])
+    _S = S / (S**2 + alpha)
+    _S[cond] = 0.0
+    VT *= T(_S)
+    
+    theta_hat = T(VT).matmul(  
+                            T(U).matmul(  ravel(y, U)  )
+                            )
+    return theta_hat
+
+
+def qr_stats(Q, R):
+    '''
+    XTX^-1  =  RT * R
+    
+    h = diag  Q * QT
+    
+    mean(h) used for normalized leverage
+    '''
+    XTX = T(R).matmul(R)
+    _XTX = pinv(XTX)
+    ## Einsum is slow in pytorch so revert to numpy version
+    h = einsum('ij,ij->i', Q, Q )
+    h_mean = h.mean()
+    
+    return _XTX, h, h_mean
+
+
+def svd_stats(U, S, VT):
+    '''
+                  1
+    XTX^-1 =  V ----- VT 
+                 S^2
+    
+    h = diag U * UT
+    
+    mean(h) used for normalized leverage
+    '''
+    _S2 = 1.0 / (S**2)
+    VS = T(VT) * _S2
+    _XTX = VS.matmul(VT)
+    h = einsum('ij,ij->i', U, U )
+    h_mean = h.mean()
+    
+    return _XTX, h, h_mean
+
+
+def ridge_stats(U, S, VT, alpha = 1):
+    '''
+                               S^2
+    exp_theta_hat =  diag V --------- VT
+                            S^2 + aI
+                            
+                                 S^2
+    var_theta_hat =  diag V ------------- VT
+                            (S^2 + aI)^2
+    
+                    1
+    XTX^-1 =  V --------- VT
+                S^2 + aI
+                
+                  S^2
+    h = diag U --------- UT
+                S^2 + aI
+    
+    mean(h) used for normalized leverage
+    '''
+    V = T(VT)
+    S2 = S**2
+    S2_alpha = S2 + alpha
+    S2_over_S2 = S2 / S2_alpha
+    
+    VS = V * S2_over_S2
+    exp_theta_hat = einsum('ij,ji->i', VS, VT )     # Same as VS.dot(VT)
+    
+    V_S2 = VS / S2_alpha  # np_divide(S2,  np_square( S2 + alpha ) )
+    var_theta_hat = einsum('ij,ij->i',  V_S2  , V )   # Sams as np_multiply(   V,   V_S2 ).sum(1)
+    
+    _XTX = (V * (1.0 / S2_alpha )  ).matmul( VT )   # V  1/S^2 + a  VT
+    
+    h = einsum('ij,ij->i', (U * S2_over_S2), U )  # Same as np_multiply(  U*S2_over_S2, U ).sum(1)
+    h_mean = h.mean()
+    
+    return exp_theta_hat, var_theta_hat, _XTX, h_mean
+
