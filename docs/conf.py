1f9c6c1ee4f8bd2b2d8eb26e87b0a05c4e4872bf
Daniel Han-Chen <danielhanchen@gmail.com>
2019-01-07 04:19:20 +1100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(os.path.abspath('../..'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
